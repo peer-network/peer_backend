f7238e615ff29d1c0379bc60f481bf1732e23e93
T.Ras <36780531+tasoraso@users.noreply.github.com>
2025-01-30 17:31:41 +0100

    Create SecurityHeadersMiddleware.php
    set request jason headers

---
+++ src/Middleware/SecurityHeadersMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Fawaz\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class SecurityHeadersMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+
+        return $response
+            ->withHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; object-src 'none';")
+            ->withHeader('X-Content-Type-Options', 'nosniff')
+            ->withHeader('X-Frame-Options', 'DENY')
+            ->withHeader('X-XSS-Protection', '1; mode=block')
+            ->withHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+    }
+}
