9f1a2a13a1771b8c109600f44b730a7fe5b709f9
T.Ras <36780531+tasoraso@users.noreply.github.com>
2025-01-30 17:29:52 +0100

    Create NotFoundHandler.php
    handle not found error

---
+++ src/Handler/NotFoundHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Fawaz\Handler;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LoggerInterface;
+use Slim\Psr7\Response;
+
+class NotFoundHandler
+{
+    public function __construct(
+        private LoggerInterface $logger
+    ) {
+    }
+
+    public function __invoke(ServerRequestInterface $request): ResponseInterface
+    {
+        $this->logger->info('Route not found: ' . $request->getMethod() . ' ' . $request->getUri());
+
+        $response = new Response(404);
+        $response->getBody()->write(json_encode(['error' => 'Not Found'], JSON_THROW_ON_ERROR));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+}
