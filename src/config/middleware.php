67427a0e623d2752a6b71537d01b8b07cf33df40
T.Ras <36780531+tasoraso@users.noreply.github.com>
2025-01-30 17:26:22 +0100

    Create middleware.php
    middleware functions

---
+++ src/config/middleware.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Slim\App;
+use Psr\Container\ContainerInterface;
+use Fawaz\Middleware\RateLimiterMiddleware;
+use Fawaz\Middleware\SecurityHeadersMiddleware;
+use Fawaz\RateLimiter\RateLimiter;
+use Psr\Log\LoggerInterface;
+
+return static function (App $app, ContainerInterface $container, array $settings) {
+    $app->addRoutingMiddleware();
+    $app->addBodyParsingMiddleware();
+
+    // Create the RateLimiter instance
+    $time = (int)$settings['timeLimiter'];
+    $rate = (int)$settings['rateLimiter'];
+    $path = (string)$settings['rateLimiterpath'];
+    $rateLimiter = new RateLimiter($rate, $time, $path); // ? requests per hour
+
+    // Get the logger from the container
+    $logger = $container->get(LoggerInterface::class);
+
+    // Register the RateLimiterMiddleware
+    $app->add(new RateLimiterMiddleware($rateLimiter, $logger));
+
+    // Register the SecurityHeadersMiddleware
+    $app->add(SecurityHeadersMiddleware::class);
+
+    // Add the error middleware
+    $app->addErrorMiddleware(true, true, true);
+};
