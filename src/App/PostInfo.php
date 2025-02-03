87eb7f6fe606f8089b72315925e9dc8c61f4cce8
T.Ras <36780531+tasoraso@users.noreply.github.com>
2025-02-03 12:37:48 +0100

    Create PostInfo.php
    Handler Post Info Service

---
+++ src/App/PostInfo.php
@@ -0,0 +1,222 @@
+<?php
+
+namespace Fawaz\App;
+
+use DateTime;
+use Fawaz\Filter\PeerInputFilter;
+
+class PostInfo
+{
+    protected string $postid;
+    protected string $userid;
+    protected int $likes;
+    protected int $dislikes;
+    protected int $reports;
+    protected int $views;
+    protected int $saves;
+    protected int $shares;
+    protected int $comments;
+
+    // Constructor
+    public function __construct(array $data = [])
+    {
+        $data = $this->validate($data);
+
+        $this->postid = $data['postid'];
+        $this->userid = $data['userid'];
+        $this->likes = $data['likes'] ?? 0;
+        $this->dislikes = $data['dislikes'] ?? 0;
+        $this->reports = $data['reports'] ?? 0;
+        $this->views = $data['views'] ?? 0;
+        $this->saves = $data['saves'] ?? 0;
+        $this->shares = $data['shares'] ?? 0;
+        $this->comments = $data['comments'] ?? 0;
+    }
+
+    // Array Copy methods
+    public function getArrayCopy(): array
+    {
+        $att = [
+            'postid' => $this->postid,
+            'userid' => $this->userid,
+            'likes' => $this->likes,
+            'dislikes' => $this->dislikes,
+            'reports' => $this->reports,
+            'views' => $this->views,
+            'saves' => $this->saves,
+            'shares' => $this->shares,
+            'comments' => $this->comments,
+        ];
+		return $att;
+    }
+
+    // Getter and Setter methods
+    public function getPostId(): string
+    {
+        return $this->postid;
+    }
+
+    public function setPostId(string $postid): void
+    {
+        $this->postid = $postid;
+    }
+
+    public function getOwnerId(): string
+    {
+        return $this->userid;
+    }
+
+    public function setOwnerId(string $userid): void
+    {
+        $this->userid = $userid;
+    }
+
+    public function getLikes(): int
+    {
+        return $this->likes;
+    }
+
+    public function setLikes(int $likes): void
+    {
+        $this->likes = $likes;
+    }
+
+    public function getDislikes(): int
+    {
+        return $this->dislikes;
+    }
+
+    public function setDislikes(int $dislikes): void
+    {
+        $this->dislikes = $dislikes;
+    }
+
+    public function getReports(): int
+    {
+        return $this->reports;
+    }
+
+    public function setReports(int $reports): void
+    {
+        $this->reports = $reports;
+    }
+
+    public function getViews(): int
+    {
+        return $this->views;
+    }
+
+    public function setViews(int $views): void
+    {
+        $this->views = $views;
+    }
+
+    public function getSaves(): int
+    {
+        return $this->saves;
+    }
+
+    public function setSaves(int $saves): void
+    {
+        $this->saves = $saves;
+    }
+
+    public function getShares(): int
+    {
+        return $this->shares;
+    }
+
+    public function setShares(int $shares): void
+    {
+        $this->shares = $shares;
+    }
+
+    public function getComments(): int
+    {
+        return $this->comments;
+    }
+
+    public function setComments(int $comments): void
+    {
+        $this->comments = $comments;
+    }
+
+    // Validation and Array Filtering methods
+	public function validate(array $data, array $elements = []): array
+	{
+		$inputFilter = $this->createInputFilter($elements);
+		$inputFilter->setData($data);
+
+		if ($inputFilter->isValid()) {
+			return $inputFilter->getValues();
+		}
+
+		$validationErrors = $inputFilter->getMessages();
+
+		foreach ($validationErrors as $field => $errors) {
+			$errorMessages = [];
+			$errorMessages[] = "Validation errors for $field";
+			foreach ($errors as $error) {
+				$errorMessages[] = ": $error";
+			}
+			$errorMessageString = implode("", $errorMessages);
+			
+			throw new ValidationException($errorMessageString);
+		}
+	}
+
+    protected function createInputFilter(array $elements = []): PeerInputFilter
+    {
+        $specification = [
+            'postid' => [
+                'required' => true,
+                'validators' => [['name' => 'Uuid']],
+            ],
+            'userid' => [
+                'required' => true,
+                'validators' => [['name' => 'Uuid']],
+            ],
+            'likes' => [
+                'required' => false,
+                'filters' => [['name' => 'ToInt']],
+                'validators' => [['name' => 'IsInt']],
+            ],
+            'dislikes' => [
+                'required' => false,
+                'filters' => [['name' => 'ToInt']],
+                'validators' => [['name' => 'IsInt']],
+            ],
+            'reports' => [
+                'required' => false,
+                'filters' => [['name' => 'ToInt']],
+                'validators' => [['name' => 'IsInt']],
+            ],
+            'views' => [
+                'required' => false,
+                'filters' => [['name' => 'ToInt']],
+                'validators' => [['name' => 'IsInt']],
+            ],
+            'saves' => [
+                'required' => false,
+                'filters' => [['name' => 'ToInt']],
+                'validators' => [['name' => 'IsInt']],
+            ],
+            'shares' => [
+                'required' => false,
+                'filters' => [['name' => 'ToInt']],
+                'validators' => [['name' => 'IsInt']],
+            ],
+            'comments' => [
+                'required' => false,
+                'filters' => [['name' => 'ToInt']],
+                'validators' => [['name' => 'IsInt']],
+            ],
+        ];
+
+        if ($elements) {
+            $specification = array_filter($specification, fn($key) => in_array($key, $elements, true), ARRAY_FILTER_USE_KEY);
+        }
+
+        return (new PeerInputFilter($specification));
+    }
+}
