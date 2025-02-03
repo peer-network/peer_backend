ff0a2823e89a4b740fa344d1f6e3fb583f301aeb
T.Ras <36780531+tasoraso@users.noreply.github.com>
2025-02-03 12:43:15 +0100

    Create ValidationException.php
    Exception Validator

---
+++ src/App/ValidationException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Fawaz\App;
+
+use RuntimeException;
+use Throwable;
+
+class ValidationException extends RuntimeException
+{
+    protected array $errors = [];
+
+    public function __construct(string $message = "", array $errors = [], ?Throwable $previous = null)
+    {
+        parent::__construct($message, 0, $previous);
+        $this->errors = $errors;
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+}
