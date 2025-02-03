797d681dc3455bf8eb470b83f87a57a2a8ce4e07
T.Ras <36780531+tasoraso@users.noreply.github.com>
2025-02-03 12:41:04 +0100

    Create TagPost.php
    Handler Tags Posts Table

---
+++ src/App/TagPost.php
@@ -0,0 +1,106 @@
+<?php
+namespace Fawaz\App;
+
+use DateTime;
+use Fawaz\Filter\PeerInputFilter;
+
+class TagPost
+{
+    protected int $tagid;
+    protected string $postid;
+    protected string $createdat;
+
+    // Constructor
+    public function __construct(array $data = [])
+    {
+        $data = $this->validate($data);
+
+        $this->tagid = $data['tagid'] ?? 0;
+        $this->postid = $data['postid'] ?? '';
+        $this->createdat = $data['createdat'] ?? (new DateTime())->format('Y-m-d H:i:s.u');
+    }
+
+    // Array Copy methods
+    public function getArrayCopy(): array
+    {
+        return [
+            'tagid' => $this->tagid,
+            'postid' => $this->postid,
+            'createdat' => $this->createdat,
+        ];
+    }
+
+    // Getter and Setter methods
+    public function getTagId(): int
+    {
+        return $this->tagid;
+    }
+
+    public function setTagId(int $tagId): void
+    {
+        $this->tagid = $tagId;
+    }
+
+    public function getPostId(): string
+    {
+        return $this->postid;
+    }
+
+    public function setPostId(string $postid): void
+    {
+        $this->postid = $postid;
+    }
+
+    // Validation and Array Filtering methods
+	public function validate(array $data, array $elements = []): array
+	{
+		$inputFilter = $this->createInputFilter($elements);
+		$inputFilter->setData($data);
+
+		if ($inputFilter->isValid()) {
+			return $inputFilter->getValues();
+		}
+
+		$validationErrors = $inputFilter->getMessages();
+
+		foreach ($validationErrors as $field => $errors) {
+			$errorMessages = [];
+			$errorMessages[] = "Validation errors for $field";
+			foreach ($errors as $error) {
+				$errorMessages[] = ": $error";
+			}
+			$errorMessageString = implode("", $errorMessages);
+			
+			throw new ValidationException($errorMessageString);
+		}
+	}
+
+    protected function createInputFilter(array $elements = []): PeerInputFilter
+    {
+        $specification = [
+            'tagid' => [
+                'required' => false,
+                'filters' => [['name' => 'ToInt']],
+                'validators' => [['name' => 'IsInt']],
+            ],
+            'postid' => [
+                'required' => true,
+                'validators' => [
+                    ['name' => 'Uuid']],
+            ],
+            'createdat' => [
+                'required' => true,
+                'validators' => [
+                    ['name' => 'Date', 'options' => ['format' => 'Y-m-d H:i:s.u']],
+                    ['name' => 'LessThan', 'options' => ['max' => (new DateTime())->format('Y-m-d H:i:s.u'), 'inclusive' => true]],
+                ],
+            ],
+        ];
+
+        if ($elements) {
+            $specification = array_filter($specification, fn($key) => in_array($key, $elements, true), ARRAY_FILTER_USE_KEY);
+        }
+
+        return (new PeerInputFilter($specification));
+    }
+}
