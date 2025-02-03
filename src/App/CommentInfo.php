9d72decad9b43279979fb2772df077a1d613fcc2
T.Ras <36780531+tasoraso@users.noreply.github.com>
2025-02-03 12:33:52 +0100

    Create CommentInfo.php
    Handle Like, Report Comment

---
+++ src/App/CommentInfo.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Fawaz\App;
+
+use DateTime;
+use Fawaz\Filter\PeerInputFilter;
+
+class CommentInfo
+{
+    protected string $commentid;
+    protected string $userid;
+    protected int $likes;
+    protected int $reports;
+    protected int $comments;
+
+    // Constructor
+    public function __construct(array $data = [])
+    {
+        $data = $this->validate($data);
+
+        $this->commentid = $data['commentid'];
+        $this->userid = $data['userid'];
+        $this->likes = $data['likes'] ?? 0;
+        $this->reports = $data['reports'] ?? 0;
+        $this->comments = $data['comments'] ?? 0;
+    }
+
+    // Array Copy methods
+    public function getArrayCopy(): array
+    {
+        $att = [
+            'commentid' => $this->commentid,
+            'userid' => $this->userid,
+            'likes' => $this->likes,
+            'reports' => $this->reports,
+            'comments' => $this->comments,
+        ];
+		return $att;
+    }
+
+    // Getter and Setter methods
+    public function getCommentId(): string
+    {
+        return $this->commentid;
+    }
+
+    public function setCommentId(string $commentid): void
+    {
+        $this->commentid = $commentid;
+    }
+
+    public function getOwnerId(): string
+    {
+        return $this->userid;
+    }
+
+    public function setOwnerId(string $userid): void
+    {
+        $this->userid = $userid;
+    }
+
+    public function getLikes(): int
+    {
+        return $this->likes;
+    }
+
+    public function setLikes(int $likes): void
+    {
+        $this->likes = $likes;
+    }
+
+    public function getReports(): int
+    {
+        return $this->reports;
+    }
+
+    public function setReports(int $reports): void
+    {
+        $this->reports = $reports;
+    }
+
+    public function getComments(): int
+    {
+        return $this->comments;
+    }
+
+    public function setComments(int $comments): void
+    {
+        $this->comments = $comments;
+    }
+
+    // Validation and Array Filtering methods
+	public function validate(array $data, array $elements = []): array
+	{
+		$inputFilter = $this->createInputFilter($elements);
+		$inputFilter->setData($data);
+
+		if ($inputFilter->isValid()) {
+			return $inputFilter->getValues();
+		}
+
+		$validationErrors = $inputFilter->getMessages();
+
+		foreach ($validationErrors as $field => $errors) {
+			$errorMessages = [];
+			$errorMessages[] = "Validation errors for $field";
+			foreach ($errors as $error) {
+				$errorMessages[] = ": $error";
+			}
+			$errorMessageString = implode("", $errorMessages);
+			
+			throw new ValidationException($errorMessageString);
+		}
+	}
+
+    protected function createInputFilter(array $elements = []): PeerInputFilter
+    {
+        $specification = [
+            'commentid' => [
+                'required' => true,
+                'validators' => [['name' => 'Uuid']],
+            ],
+            'userid' => [
+                'required' => true,
+                'validators' => [['name' => 'Uuid']],
+            ],
+            'likes' => [
+                'required' => false,
+                'filters' => [['name' => 'ToInt']],
+                'validators' => [['name' => 'IsInt']],
+            ],
+            'reports' => [
+                'required' => false,
+                'filters' => [['name' => 'ToInt']],
+                'validators' => [['name' => 'IsInt']],
+            ],
+            'comments' => [
+                'required' => false,
+                'filters' => [['name' => 'ToInt']],
+                'validators' => [['name' => 'IsInt']],
+            ],
+        ];
+
+        if ($elements) {
+            $specification = array_filter($specification, fn($key) => in_array($key, $elements, true), ARRAY_FILTER_USE_KEY);
+        }
+
+        return (new PeerInputFilter($specification));
+    }
+}
