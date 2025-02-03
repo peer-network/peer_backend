b5ccbddc93c667efddee6a9a2f629a2240631d7e
T.Ras <36780531+tasoraso@users.noreply.github.com>
2025-02-03 12:31:30 +0100

    Create ChatParticipantInfo.php
    Handle ChatsParticipiants Table

---
+++ src/App/ChatParticipantInfo.php
@@ -0,0 +1,126 @@
+<?php
+namespace Fawaz\App;
+
+use DateTime;
+use Fawaz\Filter\PeerInputFilter;
+
+class ChatParticipantInfo
+{
+    protected string $userid;
+    protected string $username;
+    protected string $img;
+    protected int $hasaccess;
+
+    // Constructor
+    public function __construct(array $data)
+    {
+        $data = $this->validate($data);
+
+        $this->userid = $data['userid'] ?? '';
+        $this->username = $data['username'] ?? '';
+        $this->img = $data['img'] ?? '';
+        $this->hasaccess = $data['hasaccess'] ?? 0;
+    }
+
+    // Array Copy methods
+    public function getArrayCopy(): array
+    {
+        $att = [
+            'userid' => $this->userid,
+            'username' => $this->username,
+            'img' => $this->img,
+            'hasaccess' => $this->hasaccess,
+        ];
+		return $att;
+    }
+
+    // Getter and Setter methods
+    public function getUserId(): string
+    {
+        return $this->userid;
+    }
+
+    public function getName(): string
+    {
+        return $this->username;
+    }
+
+    public function getImg(): ?string
+    {
+        return $this->img;
+    }
+
+    public function getHasAccess(): int
+    {
+        return $this->hasaccess;
+    }
+
+    // Validation and Array Filtering methods
+	public function validate(array $data, array $elements = []): array
+	{
+		$inputFilter = $this->createInputFilter($elements);
+		$inputFilter->setData($data);
+
+		if ($inputFilter->isValid()) {
+			return $inputFilter->getValues();
+		}
+
+		$validationErrors = $inputFilter->getMessages();
+
+		foreach ($validationErrors as $field => $errors) {
+			$errorMessages = [];
+			$errorMessages[] = "Validation errors for $field";
+			foreach ($errors as $error) {
+				$errorMessages[] = ": $error";
+			}
+			$errorMessageString = implode("", $errorMessages);
+			
+			throw new ValidationException($errorMessageString);
+		}
+	}
+
+    protected function createInputFilter(array $elements = []): PeerInputFilter
+    {
+        $specification = [
+            'userid' => [
+                'required' => true,
+                'validators' => [['name' => 'Uuid']],
+            ],
+			'username' => [
+				'required' => true,
+				'filters' => [['name' => 'StringTrim'], ['name' => 'StripTags'], ['name' => 'EscapeHtml'], ['name' => 'HtmlEntities'], ['name' => 'SqlSanitize']],
+				'validators' => [
+					['name' => 'StringLength', 'options' => [
+						'min' => 3,
+						'max' => 23,
+					]],
+					['name' => 'isString'],
+				],
+			],
+            'img' => [
+                'required' => false,
+                'filters' => [['name' => 'StringTrim'], ['name' => 'EscapeHtml'], ['name' => 'HtmlEntities'], ['name' => 'SqlSanitize']],
+                'validators' => [
+                    ['name' => 'StringLength', 'options' => [
+                        'min' => 30,
+                        'max' => 100,
+                    ]],
+                    ['name' => 'isString'],
+                ],
+            ],
+            'hasaccess' => [
+                'required' => true,
+                'filters' => [['name' => 'ToInt']],
+                'validators' => [
+                    ['name' => 'validateIntRange', 'options' => ['min' => 0, 'max' => 10]],
+                ],
+            ],
+        ];
+
+        if ($elements) {
+            $specification = array_filter($specification, fn($key) => in_array($key, $elements, true), ARRAY_FILTER_USE_KEY);
+        }
+
+		return (new PeerInputFilter($specification));
+    }
+}
