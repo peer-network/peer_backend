37db25f86d96a63c5b3781b69ef9c7cc9f345742
T.Ras <36780531+tasoraso@users.noreply.github.com>
2025-02-03 12:40:10 +0100

    Create Status.php
    Handler Users Status

---
+++ src/App/Status.php
@@ -0,0 +1,33 @@
+<?php
+namespace Fawaz\App;
+
+final class Status {
+
+	const NORMAL = 0;
+	const SUSPENDED = 1;
+	const ARCHIVED = 2;
+	const BANNED = 3;
+	const LOCKED = 4;
+	const PENDING_REVIEW = 5;
+
+	public static function getMap() : array
+	{
+		$reflectionClass = new \ReflectionClass(static::class);
+		return \array_flip($reflectionClass->getConstants());
+	}
+
+	public static function getNames() : array 
+	{
+		$reflectionClass = new \ReflectionClass(static::class);
+		return \array_keys($reflectionClass->getConstants());
+	}
+
+	public static function getValues() : array 
+	{
+		$reflectionClass = new \ReflectionClass(static::class);
+		return \array_values($reflectionClass->getConstants());
+	}
+
+	private function __construct() {}
+
+}
