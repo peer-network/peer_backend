15c8e2d8a5d9c7d72fc3e5c37af8aacc5da04cd3
Bipin Parmar <66408694+bipin1611@users.noreply.github.com>
2025-03-11 14:52:20 +0100

    fix(umlauts): fix umlauts bugs on api response

---
+++ src/Middleware/HtmlentityDecoderMiddleware.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Fawaz\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Psr7\Stream;
+
+class HtmlentityDecoderMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+
+        // Get response body content as a string
+        $body = (string) $response->getBody();
+
+        // Decode JSON
+        $data = json_decode($body, true);
+
+        // If valid JSON, decode HTML entities in all strings
+        if ($data !== null) {
+            $data = $this->decodeHtmlEntities($data);
+
+            $decodedBody = json_encode($data);
+
+            $stream = fopen('php://temp', 'r+');
+            fwrite($stream, $decodedBody);
+            rewind($stream);
+
+            return $response->withBody(new Stream($stream));
+        }
+
+        return $response;
+    }
+
+    /**
+     * Recursively decode HTML entities in all string values
+     */
+    private function decodeHtmlEntities($data)
+    {
+        if (is_array($data)) {
+            foreach ($data as $key => &$value) {
+                $value = $this->decodeHtmlEntities($value);
+            }
+        } elseif (is_string($data)) {
+            $data = html_entity_decode(htmlspecialchars_decode($data), ENT_NOQUOTES, 'UTF-8');
+        }
+
+        return $data;
+    }
+}
