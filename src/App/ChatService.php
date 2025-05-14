b6d05a49e658511442e6e230cd93b3e5515bb332
ktshn1k <ktshnik2019@gmail.com>
2025-05-14 14:09:33 +0200

    fix(GetChat): return success and code 21801 when no chat is found

--- src/App/ChatService.php
+++ src/App/ChatService.php
@@ -637,9 +637,14 @@ class ChatService
 
             $result = $this->chatMapper->loadChatById($this->currentUserId, $args);
 
-            if ($result['status'] !== 'success') {
-                throw new ValidationException($result['ResponseCode']);
-            }
+           // WE INSERT this check here
+if (empty($result['data']) || empty($result['data']['chat'])) {
+    return [
+        'status' => 'success',
+        'ResponseCode' => 21801,
+        'data' => null,
+    ];
+    }
 
             $chatData = $result['data'];
 
