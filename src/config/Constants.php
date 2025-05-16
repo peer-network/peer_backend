2615f5a453478091ce9be7bf082e3c7c3927a4e4
Dmitrii Grigorev <vid.kazan@gmail.com>
2025-05-16 13:41:31 +0200

    feat(codes-manager): add class to import, validate, export responseCodes file

---
+++ src/config/Constants.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Fawaz\config;
+
+class Constants {
+    static $pathResponseCodesFileToAssets = "/Users/fcody/Desktop/Peer/peer_backend/runtime-data/assets/";
+    static $outputResponseCodesFileName = "output.json";
+    static $inputResponseCodesFileName = "responseCodesForEditing.json";
+}
