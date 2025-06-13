d264e4dc905aaf6c23ff9ade7338823ade563f0c
Dmitrii Grigorev <vid.kazan@gmail.com>
2025-06-13 14:13:27 +0200

    feat(deps): add phinx dep, config file

---
+++ phinx.php
@@ -0,0 +1,41 @@
+<?php
+
+return
+[
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        // 'production' => [
+        //     'adapter' => 'pgsql',
+        //     'host' => 'localhost',
+        //     'name' => 'peer',
+        //     'user' => 'postgres',
+        //     'pass' => 'test',
+        //     'port' => '5432',
+        //     'charset' => 'utf8',
+        // ],
+        'development' => [
+            'adapter' => 'pgsql',
+            'host' => 'localhost',
+            'name' => 'peer',
+            'user' => 'postgres',
+            'pass' => 'test',
+            'port' => '5432',
+            'charset' => 'utf8',
+        ],
+        'testing' => [
+            'adapter' => 'pgsql',
+            'host' => 'localhost',
+            'name' => 'peer',
+            'user' => 'postgres',
+            'pass' => 'test',
+            'port' => '5432',
+            'charset' => 'utf8',
+        ]
+    ],
+    'version_order' => 'creation'
+];
