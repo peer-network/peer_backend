43b075346126845e19ff856f137ce3e102606084
Dmitrii Grigorev <vid.kazan@gmail.com>
2025-06-26 12:26:46 +0200

    reafactor(hashObject): rename

--- src/Utils/HashObject.php
+++ src/Utils/HashObject.php
@@ -5,7 +5,7 @@ namespace Fawaz\Utils;
 use Fawaz\Database\Interfaces\Hashable;
 
 trait HashObject {
-    public function hashObject(Hashable $object): string {
+    private function hashObject(Hashable $object): string {
         return hash('sha256', $object->getHashableContent());
     }
 }
