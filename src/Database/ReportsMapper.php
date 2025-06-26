2d351a4c57e6ceb9b25ea7724deff6bfed42db64
Dmitrii Grigorev <vid.kazan@gmail.com>
2025-06-26 11:05:29 +0200

    feat(reportsMapper): add hash to addReport

--- src/Database/ReportsMapper.php
+++ src/Database/ReportsMapper.php
@@ -33,8 +33,13 @@ class ReportsMapper
         // To be implemented
     }
 
-    public function addReport(string $reporter_userid, ReportTargetType $targettype, string $targetid, string $message): bool
-    {
+    public function addReport(
+        string $reporter_userid, 
+        ReportTargetType $targettype, 
+        string $targetid, 
+        string $contentHash, 
+        ?string $message = NULL
+    ): bool {
 
         $this->logger->info("ReportsMapper.addReports started");
 
@@ -45,24 +50,51 @@ class ReportsMapper
         // }
 
         $targetTypeString = $targettype->value;
-        $debugData = ['reporter_userid' => $reporter_userid, 'targetid' => $targetid,'targettype' => $targetTypeString];
+        $debugData = [
+            'reporter_userid' => $reporter_userid, 
+            'targetid' => $targetid,
+            'targettype' => $targetTypeString
+        ];
         
         try {
             $this->db->beginTransaction();
 
             // Check if the record already exists
-            $sqlCheck = "SELECT COUNT(*) FROM user_reports WHERE reporter_userid = :reporter_userid AND targetid = :targetid AND targettype = :targettype";
+            $sqlCheck = "SELECT COUNT(*) 
+                FROM user_reports 
+                WHERE reporter_userid = :reporter_userid 
+                AND targetid = :targetid 
+                AND targettype = :targettype
+                AND hash_content_sha256 = :hash_content_sha256
+            ";
             $stmtCheck = $this->db->prepare($sqlCheck);
             $stmtCheck->bindValue(':reporter_userid', $reporter_userid, \PDO::PARAM_STR);
             $stmtCheck->bindValue(':targetid', $targetid, \PDO::PARAM_STR);
             $stmtCheck->bindValue(':targettype', $targetTypeString, \PDO::PARAM_STR);
+            $stmtCheck->bindValue(':hash_content_sha256', $contentHash, \PDO::PARAM_STR);
             $stmtCheck->execute();
             $exists = $stmtCheck->fetchColumn() > 0;
 
             if (!$exists) {
                 $createdat = (string)(new DateTime())->format('Y-m-d H:i:s.u');
                 // Insert a new record
-                $sql = "INSERT INTO user_reports (reportid, reporter_userid, targetid, targettype, collected, createdat) VALUES (:reportid, :reporter_userid, :targetid, :targettype, :collected, :createdat)";
+                $sql = "INSERT INTO user_reports (
+                    reportid, 
+                    reporter_userid, 
+                    targetid, 
+                    targettype, 
+                    collected, 
+                    createdat,
+                    hash_content_sha256
+                ) VALUES (
+                    :reportid, 
+                    :reporter_userid, 
+                    :targetid, 
+                    :targettype, 
+                    :collected, 
+                    :createdat,
+                    :hash_content_sha256
+                )";
                 $stmt = $this->db->prepare($sql);
                 $stmt->bindValue(':reportid', $reportId, \PDO::PARAM_STR);
                 $stmt->bindValue(':reporter_userid', $reporter_userid, \PDO::PARAM_STR);
@@ -70,6 +102,7 @@ class ReportsMapper
                 $stmt->bindValue(':targettype', $targetTypeString, \PDO::PARAM_STR);
                 $stmt->bindValue(':collected', 0, \PDO::PARAM_STR);
                 $stmt->bindValue(':createdat', $createdat, \PDO::PARAM_STR);
+                $stmt->bindValue(':hash_content_sha256', $contentHash, \PDO::PARAM_STR);
 
                 $success = $stmt->execute();
 
