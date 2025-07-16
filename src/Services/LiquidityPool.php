adb6c4b53b35e79aea0aa370a902aeb723864d8f
Ali Abbasov <abb.ali2710@gmail.com>
2025-07-16 17:01:19 +0200

    Fix: resolve some of the level 4 issues in the code

--- src/Services/LiquidityPool.php
+++ src/Services/LiquidityPool.php
@@ -16,17 +16,6 @@ class LiquidityPool
     // Method to return accounts or environment data with a status
     public function returnAccounts(): array
     {
-        $response = $this->envi;
-
-        if (is_array($response)) 
-        {
-            // Return success status along with the data
-            return ['status' => 'success', 'response' => $response];
-        }
-        else 
-        {
-            // Return error status with an empty array if not an array
-            return ['status' => 'error', 'response' => []];
-        }
+        return ['status' => 'success', 'response' => $this->envi];
     }
 }
