65a70272eb69312f6706b290eabcf7b2e39db44f
Ali Abbasov <abb.ali2710@gmail.com>
2025-08-04 15:40:30 +0200

    Small Fix(checker)

--- src/config/checker.php
+++ src/config/checker.php
@@ -21,8 +21,7 @@ if (!in_array($db_driver, ['postgres'])) {
 
 if ($db_driver === 'postgres') {
     function is_pg_server_running($host, $port) {
-        $connection = @fsockopen($host, $port, $errno, $errstr, 5);
-
+        $connection = @fsockopen($host, (int)$port, $errno, $errstr, 5);
         if ($connection) {
             fclose($connection);
             return true;
