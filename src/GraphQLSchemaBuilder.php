19b2931c2f6657562e54bf6e9980dc4b6cd40eca
Bipin Parmar <66408694+bipin1611@users.noreply.github.com>
2025-08-12 15:17:48 +0200

    feat(refactor to its service): extract mint logic to its own service

--- src/GraphQLSchemaBuilder.php
+++ src/GraphQLSchemaBuilder.php
@@ -28,6 +28,7 @@ const PRICEDISLIKE=5;
 const PRICECOMMENT=0.5;
 const PRICEPOST=20;
 
+use Fawaz\App\AlphaMintService;
 use Fawaz\App\Chat;
 use Fawaz\App\ChatService;
 use Fawaz\App\Comment;
@@ -72,6 +73,7 @@ class GraphQLSchemaBuilder
     protected ?int $userRoles = 0;
 
     public function __construct(
+        protected AlphaMintService $alphaMintService,
         protected LoggerInterface $logger,
         protected UserMapper $userMapper,
         protected TagService $tagService,
@@ -164,6 +166,7 @@ class GraphQLSchemaBuilder
         $this->walletService->setCurrentUserId($userid);
         $this->peerTokenService->setCurrentUserId($userid);
         $this->tagService->setCurrentUserId($userid);
+        $this->alphaMintService->setCurrentUserId($userid);
     }
 
     protected function getStatusNameByID(int $status): ?string
@@ -1800,7 +1803,7 @@ class GraphQLSchemaBuilder
             'getActionPrices' => fn(mixed $root, array $args) => $this->resolveActionPrices(),
             'getTransactionHistory' => fn(mixed $root, array $args) => $this->transactionsHistory($args),
             'postInteractions' => fn(mixed $root, array $args) => $this->postInteractions($args),
-            'alphaMint' => fn(mixed $root, array $args) => $this->userService->alphaMint($args),
+            'alphaMint' => fn(mixed $root, array $args) => $this->alphaMintService->alphaMint($args),
         ];
     }
 
