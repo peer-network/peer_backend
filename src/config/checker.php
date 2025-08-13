9bb390601e0a72bee9f46175ac13197358081c78
Ali Abbasov <abb.ali2710@gmail.com>
2025-08-13 09:19:28 +0200

    Apply strict_types=1 in config/checker.php

--- src/config/checker.php
+++ src/config/checker.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 $db_driver = $_ENV['DB_DRIVER'] ?? '';
 $host = $_ENV['DB_HOST'] ?? '';
@@ -20,7 +21,7 @@ if (!in_array($db_driver, ['postgres'])) {
 
 if ($db_driver === 'postgres') {
     function is_pg_server_running($host, $port) {
-        $connection = @fsockopen($host, $port, $errno, $errstr, 5);
+        $connection = @fsockopen($host, (int)$port, $errno, $errstr, 5);
 
         if ($connection) {
             fclose($connection);
