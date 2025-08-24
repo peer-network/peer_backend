e4c8d978fea469ef391d57ad9abbb57e55f082b7
T.Ras <36780531+tasoraso@users.noreply.github.com>
2025-08-24 18:06:33 +0200

    Update SQL Posts.Status Default: 0

--- sql_files_for_import/20250824000000_posts_status.sql
+++ sql_files_for_import/20250824000000_posts_status.sql
@@ -1,10 +1,12 @@
 BEGIN;
 
--- Table: Posts
--- Update Posts Set the default value of column status
+-- Default ändern
 ALTER TABLE posts 
-ALTER COLUMN status SET DEFAULT 0;
--- Update all posts has been set to 10 befor
-UPDATE posts SET status = 0 WHERE status = 10;
+    ALTER COLUMN status SET DEFAULT 0;
+
+-- Nur die bisherigen Werte von 10 auf 0 setzen
+UPDATE posts 
+    SET status = 0
+    WHERE status = 10;
 
 COMMIT;
