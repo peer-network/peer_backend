62d8ce439816aecd42da069116d83dc9f232f22f
Dmitrii Grigorev <vid.kazan@gmail.com>
2025-08-28 09:13:53 +0200

    chore(rector): add rector.php

--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -12,11 +13,13 @@ return RectorConfig::configure()
         __DIR__ . '/tests',
     ])
     // uncomment to reach your current PHP version
-    // ->withPhpSets()
-    ->withTypeCoverageLevel(0)
-    ->withDeadCodeLevel(0)
-    ->withCodeQualityLevel(0);
-    // ->withPreparedSets(
-    //     deadCode: true,
-    //     codeQuality: true
-    // );
+     ->withRules([
+        TypedPropertyFromStrictConstructorRector::class
+    ])
+    // ->withTypeCoverageLevel(0)
+    // ->withDeadCodeLevel(0)
+    // ->withCodeQualityLevel(0);
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true
+    );
