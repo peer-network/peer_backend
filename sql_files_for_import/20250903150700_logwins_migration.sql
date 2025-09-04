178e2ef82bff12fea6e5a36ae9c8478cc831f749
Dmitrii Grigorev <vid.kazan@gmail.com>
2025-09-04 14:21:57 +0200

    fix(migration): logwins migration is idempotent

--- sql_files_for_import/20250903150700_logwins_migration.sql
+++ sql_files_for_import/20250903150700_logwins_migration.sql
@@ -1,12 +1,23 @@
-BEGIN;
+DO $$
+BEGIN
+    -- Prüfen, ob die Spalte 'migrated' bereits existiert
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.columns 
+        WHERE table_name = 'logwins' 
+          AND column_name = 'migrated'
+    ) THEN
+        -- Spalte hinzufügen
+        ALTER TABLE logwins 
+            ADD COLUMN migrated INT DEFAULT 1;
 
-ALTER TABLE logwins 
-    ADD COLUMN migrated INT DEFAULT 1;
-    
--- Update all records to unmigrated
-UPDATE logwins SET migrated = 0;
+        -- Alle Zeilen auf unmigrated setzen
+        UPDATE logwins 
+        SET migrated = 0;
 
--- Exclude Alpha User
-UPDATE logwins SET migrated = 1 WHERE fromid = '2736677b-57b8-4ee2-87fe-24ed975e55a6';
-
-COMMIT;
+        -- Alpha User ausschließen
+        UPDATE logwins 
+        SET migrated = 1 
+        WHERE fromid = '2736677b-57b8-4ee2-87fe-24ed975e55a6';
+    END IF;
+END$$;
