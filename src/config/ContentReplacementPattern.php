8ce74aff3e1d90e07405931c51e3dda916b929e9
Dmitrii Grigorev <vid.kazan@gmail.com>
2025-11-11 11:31:30 +0100

    feat(hidden-content): show all content

--- src/config/ContentReplacementPattern.php
+++ src/config/ContentReplacementPattern.php
@@ -19,64 +19,64 @@ enum ContentReplacementPattern: string
             self::illegal => null,
         };
     }
-    public function postTitle(): string
+    public function postTitle(): ?string
     {
         return match ($this) {
-            self::hidden => "this post is hidden",
+            self::hidden   => null,
             self::deleted => "this post is deleted",
             self::illegal => "this post is illegal",
         };
     }
 
-    public function postDescription(): string
+    public function postDescription(): ?string
     {
         return match ($this) {
-            self::hidden   => "",
+            self::hidden   => null,
             self::deleted => "",
             self::illegal => "",
         };
     }
 
-    public function postMedia(): string
+    public function postMedia(): ?string
     {
         return match ($this) {
-            self::hidden   => "some_pic_to_be_here_some_text_to_pass_validation",
+            self::hidden   => null,
             self::deleted => "some_pic_to_be_here_some_text_to_pass_validation",
             self::illegal => "some_pic_to_be_here_some_text_to_pass_validation",
         };
     }
 
-    public function commentContent(): string
+    public function commentContent(): ?string
     {
         return match ($this) {
-            self::hidden   => "this comment is hidden",
+            self::hidden   => null,
             self::deleted => "this comment is deleted",
             self::illegal => "this comment is illegal",
         };
     }
 
-    public function username(): string
+    public function username(): ?string
     {
         return match ($this) {
-            self::hidden   => "hidden_account",
+            self::hidden   => null,
             self::deleted => "Deleted_Account",
             self::illegal => "illegal_account",
         };
     }
 
-    public function userBiography(): string
+    public function userBiography(): ?string
     {
         return match ($this) {
-            self::hidden   => "/userData/00000000-0000-0000-0000-000000000000.txt",
+            self::hidden   => null,
             self::deleted => "/userData/00000000-0000-0000-0000-000000000000.txt",
             self::illegal => "/userData/00000000-0000-0000-0000-000000000000.txt",
         };
     }
 
-    public function profilePicturePath(): string
+    public function profilePicturePath(): ?string
     {
         return match ($this) {
-            self::hidden   => "/profile/00000000-0000-0000-0000-000000000000.jpeg",
+            self::hidden   => null,
             self::deleted => "/profile/00000000-0000-0000-0000-000000000000.jpeg",
             self::illegal => "/profile/00000000-0000-0000-0000-000000000000.jpeg",
         };
