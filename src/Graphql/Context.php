9221b8cb17c0cada67409be33468fc14f3aca9ff
Dmitrii Grigorev <vid.kazan@gmail.com>
2025-12-07 12:31:21 +0100

    refactor(resolver): add Context data type which contains basic user/request data for resolvers

---
+++ src/Graphql/Context.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fawaz\GraphQL;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Per-request GraphQL context passed to resolvers.
+ */
+class Context
+{
+    /** @param array<string,mixed> $dataloaders */
+    public function __construct(
+        public readonly ServerRequestInterface $request,
+        public readonly ?array $user,
+        public readonly ?string $token,
+        public readonly array $dataloaders = []
+    ) {
+    }
+}
+
