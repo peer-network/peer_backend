07b35fbe462c67ba04642a67056b267c3518b6f8
Dmitrii Grigorev <vid.kazan@gmail.com>
2025-12-07 12:57:04 +0100

    refactor(user-resolvers): move resolvers

--- src/Graphql/Resolvers/UserMutationResolver.php
+++ src/Graphql/Resolvers/UserMutationResolver.php
@@ -10,6 +10,7 @@ use Fawaz\App\UserService;
 use Fawaz\App\UserInfoService;
 use Fawaz\App\CommentInfoService;
 use Fawaz\GraphQL\Support\ResolverHelpers;
+use Fawaz\Utils\ResponseHelper;
 
 /**
  * Mutation resolvers for user-related actions.
@@ -17,6 +18,8 @@ use Fawaz\GraphQL\Support\ResolverHelpers;
 class UserMutationResolver implements ResolverProvider
 {
     use ResolverHelpers;
+    use ResponseHelper;
+
     public function __construct(
         private readonly PeerLoggerInterface $logger,
         private readonly UserService $userService,
@@ -41,7 +44,52 @@ class UserMutationResolver implements ResolverProvider
                 'likeComment' => $this->withAuth(null, fn (mixed $root, array $args) => $this->commentInfoService->likeComment($args['commentid'])),
                 'reportComment' => $this->withAuth(null, fn (mixed $root, array $args) => $this->commentInfoService->reportComment($args['commentid'])),
                 'reportUser' => $this->withAuth(null, fn (mixed $root, array $args) => $this->userInfoService->reportUser($args['userid'])),
+                'register' => $this->withAuth(null,fn (mixed $root, array $args) => $this->createUser($args['input'])),
+                'listBlockedUsers' => $this->withAuth(
+                    null,
+                    $this->withValidation(
+                        [], 
+                        fn (mixed $root, array $args) => $this->resolveBlocklist($args)
+                    )
+                ),
             ],
         ];
+    }
+
+    protected function createUser(array $args): ?array
+    {
+        $this->logger->debug('Query.createUser started');
+
+        $response = $this->userService->createUser($args);
+        if (isset($response['status']) && $response['status'] === 'error') {
+            return $response;
+        }
+
+        if (!empty($response)) {
+            return $response;
+        }
+
+        $this->logger->error('Query.createUser No data found');
+        return $this::respondWithError(41105);
+    }
+
+    protected function resolveBlocklist(array $args): ?array {
+        $this->logger->debug('Query.resolveBlocklist started');
+
+        $response = $this->userInfoService->loadBlocklist($args);
+        if (isset($response['status']) && $response['status'] === 'error') {
+            return $response;
+        }
+
+        if (empty($response['counter'])) {
+            return $this::createSuccessResponse(11107, [], false);
+        }
+
+        if (is_array($response) || !empty($response)) {
+            return $response;
+        }
+
+        $this->logger->error('Query.resolveBlocklist No data found');
+        return $this::respondWithError(41105);
     }
 }
