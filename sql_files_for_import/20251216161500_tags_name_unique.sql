ca3d8cf5e48e80d1cf75ea0c6335a05596aad141
Ali Abbasov <abb.ali2710@gmail.com>
2025-12-18 14:10:35 +0100

    Set tagname unique and add index by name

--- sql_files_for_import/20251216161500_tags_name_unique.sql
+++ sql_files_for_import/20251216161500_tags_name_unique.sql
@@ -1,6 +1,10 @@
 BEGIN;
 
-CREATE UNIQUE INDEX IF NOT EXISTS tags_name_unique_idx
+ALTER TABLE tags
+    ADD CONSTRAINT tags_name_unique
+    UNIQUE (name);
+
+CREATE INDEX IF NOT EXISTS idx_tags_name
     ON tags (name);
 
 COMMIT;
