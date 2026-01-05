0791f61797f763a8e8b99be34954854f06c0972a
Bipin Parmar <66408694+bipin1611@users.noreply.github.com>
2026-01-05 16:42:41 +0100

    feat(add sql to lp): add lp fees back to recipient

---
+++ db/ops_scripts/20260105000000_update_wallett_balance_with_removed_lp_fees.sql
@@ -0,0 +1,24 @@
+-- This script updates the 'wallett' table by adding back the liquidity fees
+-- that were removed from transactions of type 'transferSenderToRecipient'
+
+DO $$
+DECLARE
+  r RECORD;
+  fee numeric;
+  net numeric;
+BEGIN
+  FOR r IN
+    SELECT recipientid, tokenamount
+    FROM public.transactions
+    WHERE transactiontype = 'transferSenderToRecipient'
+      AND createdat <= TIMESTAMP '2025-12-09 17:08:00'
+    ORDER BY createdat ASC
+  LOOP
+	net := r.tokenamount / 1.01;
+    fee := r.tokenamount - net;
+
+    UPDATE public.wallett
+    SET liquidity = liquidity + fee
+    WHERE userid = r.recipientid;
+  END LOOP;
+END $$;
