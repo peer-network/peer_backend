dd8af018d1a7241d388fcf1bc5d543d50b58ea03
Dmitrii Grigorev <vid.kazan@gmail.com>
2026-01-27 17:12:11 +0100

    feat(server-migration): fix migration script

--- 2-ovh-backend-apply_server_archive.sh
+++ 2-ovh-backend-apply_server_archive.sh
@@ -1,8 +1,16 @@
+echo "Installing pv..."
 sudo apt install pv
 
-scp -i ~/.ssh/old-staging ubuntu@80.158.61.213:~/staging-server-data-20260121.tar.gz ./
-tar -xzf staging-server-data-20260121.tar.gz -C ./staging-server-data-20260121
+#echo "Downloading server archive..."
+#scp -r -i ./old-staging-ssh-key ubuntu@80.158.61.213:~/staging-server-data-202601260837 ./
+echo "Extracting runtime-data..."
+sudo tar -xf ./staging-server-data-202601260837/staging-server-data-202601260837.tar -C ./staging-server-data-202601260837
 
-pg_restore  -h 10.5.2.186   -U postgres  -d peer-old-staging-20260121   --clean --if-exists --no-owner  ./staging-20260121.dump
+#echo "Creating database..."
+#psql -h 192.168.1.210 -U postgres -tc "SELECT 1 FROM pg_database WHERE datname='staging-server-data-202601260837'" | grep -q 1 || psql -h 192.168.1.210 -U postgres -c "CREATE DATABASE \"staging-server-data-202601260837\""
+#echo "Restoring database..."
+#pg_restore  -h 192.168.1.210 -U postgres  -d staging-server-data-202601260837    --clean --if-exists --no-owner --no-privileges  ./staging-server-data-202601260837/staging-202601260837.dump
 
-sudo rsync -av --progress /var/www/peer_beta/peer_backend/runtime-data ./staging_runtime_data_20260121 
+echo "Syncing runtime-data..."
+sudo mkdir /var/www/peer_backend/runtime-data-202601260837
+sudo rsync -av --progress staging-server-data-202601260837/var/www/peer_beta/peer_backend/runtime-data/ /var/www/peer_backend/runtime-data-202601260837
