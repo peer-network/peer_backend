18f32521d413a4c40e7290097e4d6029dbd5c913
Bipin Parmar <66408694+bipin1611@users.noreply.github.com>
2026-01-28 12:11:56 +0100

    feat(title search): add title search on ads

--- src/Database/AdvertisementMapper.php
+++ src/Database/AdvertisementMapper.php
@@ -841,11 +841,16 @@ class AdvertisementMapper
         $tag    = $args['tag']    ?? null;
         $postId = $args['postid'] ?? null;
         $userId = $args['userid'] ?? null;
+        $title    = $args['title']    ?? null;
 
         $whereClauses[] = "p.feedid IS NULL";
         $params['currentUserId'] = $currentUserId;
 
 
+        if ($title !== null) {
+            $whereClauses[] = "p.title ILIKE :title";
+            $params['title'] = '%' . $title . '%';
+        }
 
         if ($postId !== null) {
             $whereClauses[] = "p.postid = :postId";
