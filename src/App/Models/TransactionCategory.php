8a35c5678720844a33835735edf32a6288061cdb
Bipin Parmar <66408694+bipin1611@users.noreply.github.com>
2026-02-10 13:23:23 +0100

    feat(tnx history): add new inviter category and fix tnx history

--- src/App/Models/TransactionCategory.php
+++ src/App/Models/TransactionCategory.php
@@ -7,6 +7,7 @@ namespace Fawaz\App\Models;
 enum TransactionCategory: string
 {
     case P2P_TRANSFER = 'P2P_TRANSFER';
+    case INVITER_FEE_EARN = 'INVITER_FEE_EARN';
     case AD_PINNED = 'AD_PINNED';
     case POST_CREATE = 'POST_CREATE';
     case LIKE = 'LIKE';
